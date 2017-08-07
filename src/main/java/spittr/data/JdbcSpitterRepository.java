c6e27f12dd64faa95c9538dfa1cef8b6b853fd8b
sagittaria <shuyang_x@hotmail.com>
2017-08-07 19:54:33 +0800

    用jdbcTemplate获取数据findByUsername-达成预期

--- src/main/java/spittr/data/JdbcSpitterRepository.java
+++ src/main/java/spittr/data/JdbcSpitterRepository.java
@@ -1,7 +1,11 @@
 package spittr.data;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import spittr.Spitter;
@@ -11,6 +15,9 @@ public class JdbcSpitterRepository implements SpitterRepository {
 
     private static final String INSERT_SPITTER = "insert into Spitter (id, firstname, "
             + "lastname,username, password) values (?, ?, ?, ?, ?)";
+    
+    private static final String SELECT_SPITTER_BY_USERNAME = "select id, firstname, lastname,"
+            + "username, password from spitter where username=?";
     
     private JdbcOperations jdbcOperations;
     
@@ -30,7 +37,17 @@ public class JdbcSpitterRepository implements SpitterRepository {
     }
     
     public Spitter findByUsername(String username) {
-        return null;
+        return jdbcOperations.queryForObject(SELECT_SPITTER_BY_USERNAME, new SpitterRowMapper(),username);
     }
 
+    private static final class SpitterRowMapper implements RowMapper<Spitter> {
+        public Spitter mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Spitter(rs.getLong("id"),
+                    rs.getString("firstname"),
+                    rs.getString("lastname"), 
+                    rs.getString("username"),
+                    rs.getString("password"));
+        }
+    }
+    
 }
